c08f96f38b7f3d0adf80cbd6275c3703aa017ebc
Richard Nguyen <rin002@ucsd.edu>
2025-09-23 13:54:05 -0700

    First commit, setting up backend and frontend foundations

---
+++ backend/src/main/java/com/example/project_1_meme_webpage/Project1MemeWebpageApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_1_meme_webpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1MemeWebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1MemeWebpageApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/project_1_meme_webpage/Project1MemeWebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_1_meme_webpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1MemeWebpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
